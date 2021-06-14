c05465db3d74025d370f4a4120f7af1eea0c580b
KaivalyaVernekar <vernekarkaivalya@gmail.com>
2021-06-14 20:41:06 +0530

    feat: navbar layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
@@ -9,11 +9,17 @@ import CartPage from "./pages/HomePage/HomePage";
 
 // Components
 import Navbar from "./components/Navbar/Navbar";
+import Backdrop from "./components/Backdrop/Backdrop";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <Router>
       <Navbar />
+      <SideDrawer show={sideToggle} />
+      <Backdrop show={sideToggle} />
       <main className="app">
         <Switch>
           <Route exact path="/" component={HomePage} />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,9 +13,11 @@ const Navbar = () => {
       {/* links */}
       <ul className="navbar__links">
         <li>
-          <Link to="/cart">
+          <Link to="/cart" className="cart__link">
             <i className="fas fa-shopping-cart"></i>
-            Cart <span className="cartlogo__badge"></span>
+            <span>
+              Cart <span className="cartlogo__badge">0</span>
+            </span>
           </Link>
         </li>
         <li>
